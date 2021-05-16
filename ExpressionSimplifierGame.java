899d4973aeef9329235f522cf5cee0e4e41cdf8c
Kelvin Likollari <55785978+likolk@users.noreply.github.com>
2021-05-16 17:54:39 +0200

    More methods

--- ExpressionSimplifierGame.java
+++ ExpressionSimplifierGame.java
@@ -7,5 +7,30 @@
  */
 public class ExpressionSimplifierGame
 { 
+    // the person that is going to type in the input formula
+    private User[] player;
+    private ExpressionWindow expression;
+    private User user;
+    private int x;
+    private int y;
     
+    /**
+     * Constructor
+     * 
+     */
+    public ExpressionSimplifierGame() {
+        //
+    }
+    
+    /**
+     * The method can allow to move the piece.
+     * @param userMove indicate.
+     */
+    public static void quitGame(final String userAction) { 
+       // Given a key press, close the game.
+       // use q for Quit.
+       if ("q".equals(userAction)) { 
+           System.exit(0);
+        }       
+    }
 }
