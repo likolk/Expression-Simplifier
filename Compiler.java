2abd00f954d1e30e10235f8ef6921b2b782f7a9d
Kelvin Likollari <55785978+likolk@users.noreply.github.com>
2021-05-21 08:39:19 +0200

    added token and compiler class useful for parsing the math expression

---
+++ Compiler.java
@@ -0,0 +1,15 @@
+/**
+ * A Compiler converts an AST into a compiled Program.
+ */
+public class Compiler {
+    
+    /**
+     * Compile the given AST.
+     * @param node The root of an AST
+     * @return the compiled program
+     */
+    public Program compile(final Node node) {
+        return null; // to be fixed
+    }
+    
+}
---
+++ Token.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class Token here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Token
+{
+    
+}
+    
+     
