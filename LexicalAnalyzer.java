02d6b541a3257bf0c8fe2f75bb24bb296f6a9ce8
Kelvin Likollari <55785978+likolk@users.noreply.github.com>
2021-05-21 09:02:13 +0200

    added lexical analyzer class

---
+++ LexicalAnalyzer.java
@@ -0,0 +1,37 @@
+
+/**
+ * Write a description of class LexicalAnalyzer here.
+ *
+ * @author (Kelvin Likollari)
+ * @version (a version number or a date)
+ */
+public final class LexicalAnalyzer {
+
+    private Token token;
+    private String text;
+    private int position;
+    
+    /**
+     * Create an analyzer for the given text, 
+     * using the given factories to recognize and create tokens.
+     * @param expression The text to analyze
+     * @param factories The token factories to use
+     */
+    public LexicalAnalyzer(final String expression) {
+        //
+    }
+
+    /**
+     * Get the current token.
+     * @return the current token
+     */
+    public Token getCurrentToken() {
+        return token;
+    }
+    
+    // Scan the text and extract the following token
+    public Token scanToken() {
+        return null;
+    }
+}
+     
