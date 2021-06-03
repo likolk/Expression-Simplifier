20612e1623e511926dd622b41ede1b93fe5252d0
Kelvin Likollari <55785978+likolk@users.noreply.github.com>
2021-06-03 18:13:09 +0200

    Major Changes in classes - Mega commit

---
+++ ExpressionTree.java
@@ -0,0 +1,46 @@
+public class ExpressionTree {
+    
+
+    //Tree object that is created when an expression is parsed
+    private int type;
+    private Object leafvalue;
+    public ExpressionTree leftChild, rightChild;
+    public static final int NUM = 0;
+    public static final int value = 1;
+    public static final int OP = 2;
+    // OR shorthand public static final int NUM = 0, valueue = 1, OP = 2;
+    private StringBuffer stringBuffer = new StringBuffer();
+
+    public ExpressionTree(int type, Object leafvalue, ExpressionTree l, ExpressionTree r) {
+        this.type = type;
+        this.leafvalue = leafvalue;
+        this.leftChild = l;
+        this.rightChild = r;
+    }
+    //return the forth expression, a postfix expression
+    public String toPostfix(ExpressionTree t) {
+       if (t != null) {
+           toPostfix(t.leftChild);
+            toPostfix(t.rightChild);
+            stringBuffer.append(t.leafvalue);
+            stringBuffer.append(" ");
+        }
+        return stringBuffer.toString();
+    }
+
+    public ExpressionTree getRoot() {
+        return this;
+    }
+    
+    public Object getLeafvalueue() {
+        return leafvalue;
+    }
+
+    public ExpressionTree getleftChild() {
+        return leftChild;
+    }
+    
+    public ExpressionTree getrightChild() {
+        return rightChild;
+    }
+}
---
+++ lexer.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+
+public class lexer {
+    
+    
+    
+    
+    
+    
+    
+}
