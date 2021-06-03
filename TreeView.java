cc959e37677fc979e8b844371dcc7f4d1d47134e
Kelvin Likollari <55785978+likolk@users.noreply.github.com>
2021-06-04 00:48:01 +0200

    Updated 2 classes & ReadMe file

--- TreeView.java
+++ TreeView.java
@@ -2,18 +2,64 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import javax.swing.JPanel;
-/**
- * Write a description of class TreeView here.
- *
- * @author (KL)
- * @version (a version number or a date)
- */
+
 public class TreeView extends JPanel  {
-    //Class for drawing the Tree onto a panel
-    // adjust numbers according to personal preferences.
+    // Class for drawing the Tree onto a panel
     private int radius = 20;
     private int levelGap = 50;
-    ExpressionTree t; 
-}
-
     
+    ExpTree t;
+
+    public TreeView(ExpTree t) {
+        this.t = t;
+    }
+
+    protected void paintComponent(Graphics g) {
+        g.setColor(Color.blue);
+        g.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 11));
+
+        if (t.getRoot() != null) {
+            displayTree(g, t.getRoot(), getWidth() / 2, 30, getWidth() / 4);
+        }
+    }
+
+    private void displayTree(Graphics g, ExpTree t, int x, int y, int gap) {
+        g.drawString(getText(String.valueOf(t.getLeafVal())), x - 20, y - 22);
+        //   g.drawRect(x - radius, y - radius, 2 * radius, 2 * 10);
+        g.drawRoundRect(x - radius, y - radius, 2 * radius, 2 * 10, 10, 10);
+        //  g.fillRoundRect(x - radius, y - radius, 2 * radius, 2 * 10, 10, 10);
+        g.drawString(t.getLeafVal() + "", x - 2, y - 5);
+        
+        if (t.getrChild() != null) {
+            displayTree(g, t.rChild, x + gap, y + levelGap, gap /2);
+            connectRect(g, x + gap, y + levelGap, x, y);
+        }
+        
+        if (t.getlChild() != null) {
+            connectRect(g,  x - gap , y + levelGap,  x, y);
+            displayTree(g, t.lChild, x - gap, y + levelGap, gap /2);
+        }
+    }
+    
+    private void connectRect(Graphics g, int x1, int y1, int x2, int y2) {
+        g.drawLine(x1 - 20, y1 - 20, x2, y2);
+    }
+    
+    public String getText(String op) {
+        char c = op.charAt(0);
+        String res  = null;
+        if(c == '+') {
+            res= "Addition";
+        }
+        else if (c == '-') {
+            res = "Subtraction";
+        } else if (c == '*') {
+            res = "Multiplication";
+        } else if (c == '/') {
+            res = "Division";
+        } else if ((c >= '0' && c <= '9' ) || (c >= 'a' && c <= 'z' ) || (c >= 'A' && c <= 'Z' )) {
+               res = "Literal";
+            }
+            return res;
+        }
+}
---
+++ validExpression.java
@@ -0,0 +1,101 @@
+import java.util.Stack;
+
+public class validExpression
+{
+    
+    /**
+     * @param args the command line arguments
+     */
+ 
+    static boolean checkValid(char[] equ){
+        
+        int i,j = equ.length;
+        Stack<Character> astack = new Stack<>();
+        if (j==0) {
+            return false;
+        }
+
+        for(i=0; i<j; i++){
+                   // System.out.println("?"+equ[i]);
+            if (isOpen(equ[i])){
+                if (!astack.isEmpty() && isNumber(astack.peek())) {
+                    return false;
+                }
+                astack.push(equ[i]);
+            } else if (isNumber(equ[i])){
+                astack.push(equ[i]);
+            } else if (isOp(equ[i])){
+                if (astack.isEmpty() || !isNumber(astack.peek())) {
+                    return false;
+                }
+                astack.push(equ[i]);
+            } else if (isClose(equ[i])){
+                if (astack.isEmpty() || !isNumber(astack.peek())) {
+                    return false;
+                }
+                while(!astack.isEmpty()){
+                    char temp=astack.pop();
+                    if (isOpen(temp)) {
+                        if (doMatch(temp,equ[i])) {
+                            break;
+                        } else {
+                            return false;//unexpected open
+                        }
+                    } else {
+                        if(astack.isEmpty()) {
+                            return false;
+                        }
+                    }
+                }
+                astack.push('1');
+            } else { //unexpected char
+                return false;
+            }
+        }
+        if (!astack.isEmpty() && !isNumber(astack.peek())) {
+            return false;
+        }
+        while (!astack.isEmpty()) {
+            if(isOpen(astack.pop())) {
+                return false;
+            }
+        }
+         
+        return true;
+    }
+
+    private static boolean isOpen(char c) {
+        if(c == '(' || c== '[' ) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isNumber(char c) {
+        if((c >= '0' && c <= '9' ) || (c >= 'a' && c <= 'z' ) || (c >= 'A' && c <= 'Z' )) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isOp(char c) {
+        if (c == '+' || c == '-' || c == '*' || c == '/') {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isClose(char c) {
+        if(c == ')' || c == ']') {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean doMatch(char temp, char c) {
+        if((temp == '(' && c == ')')||(temp == '[' && c == ']')) {
+            return true;
+        }
+        return false;
+    }
+}
